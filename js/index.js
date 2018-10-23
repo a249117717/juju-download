44d0933756a02462d7bff09be9214372399a2bea
刘烽 <liufeng@liufengdeiMac.local>
2018-10-23 22:51:32 +0800

    测试版本

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ var _domain = "http://39.108.151.200:9999";
         "error":function(){
 
         }
-    })
+    });
 
     // 设置头像
     img.src = search[2];
@@ -50,45 +50,29 @@ var _domain = "http://39.108.151.200:9999";
 
     // 设置分享数据
     function setUpShareData (title, description, link, imgUrl) {
-        wx.onMenuShareTimeline({
+        // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
+        wx.updateAppMessageShareData({
             title: title, // 分享标题
             desc: description,
             link: link, // 分享链接，该链接域名必须与当前企业的可信域名一致
             imgUrl: imgUrl, // 分享图标
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-            }
-        });
+            },function(res) {
 
-        wx.onMenuShareAppMessage({
+            }
+        );
+
+        // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
+        wx.updateTimelineShareData({
             title: title, // 分享标题
             desc: description,
             link: link, // 分享链接，该链接域名必须与当前企业的可信域名一致
             imgUrl: imgUrl, // 分享图标
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-            }
-        });
+            },function(res) {
 
-        wx.onMenuShareQQ({
-            title: title, // 分享标题
-            desc: description,
-            link: link, // 分享链接，该链接域名必须与当前企业的可信域名一致
-            imgUrl: imgUrl, // 分享图标
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
             }
-        });
+        );
 
+        // 腾讯微博
         wx.onMenuShareWeibo({
             title: title, // 分享标题
             desc: description,
@@ -102,6 +86,7 @@ var _domain = "http://39.108.151.200:9999";
             }
         });
 
+        // QQ空间
         wx.onMenuShareQZone({
             title: title, // 分享标题
             desc: description,
@@ -118,9 +103,10 @@ var _domain = "http://39.108.151.200:9999";
 
     // 微信分享准备就绪
     wx.ready(function(){
-        setUpShareData('我已经在等你啦，快来神秘巨星陪我玩',
-        '随时随地，一起玩游戏!',
+        setUpShareData('亲，我在神秘巨星APP等你哦~',
+        '邀请码：' + search[0] + '，来即欧皇！',
         window.location.href,
         img.src);
+        alert(1);
     });
 }());
